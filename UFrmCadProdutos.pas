a3dd559859e10bd0be084c22b31608052e65c395
Sanatiel <sanatielsg@gmail.com>
2026-09-01 21:54:19 -0300

    adicionando formulario cadastro produtos

---
+++ UFrmCadProdutos.pas
@@ -0,0 +1,24 @@
+unit UFrmCadProdutos;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TFrmCadProdutos = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FrmCadProdutos: TFrmCadProdutos;
+
+implementation
+
+{$R *.dfm}
+
+end.
