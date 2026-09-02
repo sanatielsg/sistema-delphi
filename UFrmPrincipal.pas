f5b69f922eca37594c36671c859c22c301e5e1ba
Sanatiel <sanatielsg@gmail.com>
2026-09-01 23:17:28 -0300

    criando datamodule e banco de dados

--- UFrmPrincipal.pas
+++ UFrmPrincipal.pas
@@ -5,6 +5,7 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Menus
+  , UDM
   , UFrmCadProdutos
   , UFrmCadClientes
   , UFrmCadCaixas
