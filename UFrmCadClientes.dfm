f4f15777f15ad2b700a48f52b2fc07167932361e
Sanatiel <sanatielsg@gmail.com>
2026-09-01 22:09:38 -0300

    adicionando formulario cadastro clientes

---
+++ UFrmCadClientes.dfm
@@ -0,0 +1,14 @@
+object FrmCadClientes: TFrmCadClientes
+  Left = 0
+  Top = 0
+  Caption = 'Cadastro de Clientes'
+  ClientHeight = 441
+  ClientWidth = 624
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  TextHeight = 15
+end
---
+++ UFrmCadClientes.pas
@@ -0,0 +1,24 @@
+unit UFrmCadClientes;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TFrmCadClientes = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FrmCadClientes: TFrmCadClientes;
+
+implementation
+
+{$R *.dfm}
+
+end.
